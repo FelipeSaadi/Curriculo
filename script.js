3a0a673cb339ad98bf367f0f29d35f83ebc7098a
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-05-22 16:06:49 -0300

    Implementação de listas com JQUERY
    Inserção dinâmica de dados em listas utilizando o back.

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ $(".section-bar").css({
     height: "3px",
     margin: "20px 0 20px 0"
 })
+
+let hobbies = [
+    "Ler Livros",
+    "Futebol",
+    "Estudar",
+    "Participar de Projetos",
+    "Sair com os amigos"
+]
+
+let abilities = [
+    "Linguagens (HTML, CSS, Python, Javascript e React).",
+    "Comunicação.",
+    "Trabalho em Equipe.",
+    "Liderança.",
+    "Proatividade.",
+    "Pensamento Crítico.",
+    "Escuta ativo.",
+    "Criatividade para criação de Soluções Tecnológicas"
+]
+
+hobbies.forEach(item => {
+    $("#hobbies-section ul")[0].innerHTML += `<li>${item}</li>`
+})
+
+abilities.forEach(item => {
+    $("#abilities-section ul")[0].innerHTML += `<li>${item}</li>`
+})
