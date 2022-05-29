a9ef3ceb6502faa88ea916e12bdce39ad4b2ae13
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-05-29 18:10:55 -0300

    Implementação Parcial do Banco
    Criada a estrutura do banco, inserido os dados, aplicada lógica de dados dinâmicos para todo o menu lateral.

---
+++ backend.js
@@ -0,0 +1,50 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = 'curriculo.db';
+
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static("./"));
+
+app.use(express.json());
+
+app.get('/personalData', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM PersonalData';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+app.get('/abilities', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM Abilities';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+
+/* Inicia o servidor */
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
--- script.js
+++ script.js
@@ -5,29 +5,69 @@ $(".section-bar").css({
     margin: "20px 0 20px 0"
 })
 
-let hobbies = [
-    "Ler Livros",
-    "Futebol",
-    "Estudar",
-    "Participar de Projetos",
-    "Sair com os amigos"
-]
+function getPersonalData() {
+    let url = "/personalData"
 
-let abilities = [
-    "Linguagens (HTML, CSS, Python, Javascript e React).",
-    "Comunicação.",
-    "Trabalho em Equipe.",
-    "Liderança.",
-    "Proatividade.",
-    "Pensamento Crítico.",
-    "Escuta ativo.",
-    "Criatividade para criação de Soluções Tecnológicas"
-]
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("GET", url, false)
+    xhttp.send()
 
-hobbies.forEach(item => {
-    $("#hobbies-section ul")[0].innerHTML += `<li>${item}</li>`
-})
+    let data = JSON.parse(xhttp.responseText)[0]
 
-abilities.forEach(item => {
-    $("#abilities-section ul")[0].innerHTML += `<li>${item}</li>`
-})
+    $("#photo-section")[0].innerHTML = `<img id="photo" src="${data.image_url}">`
+    $("#profile-name-title")[0].innerText = data.name.toUpperCase()
+    $("#profile-course")[0].innerText = data.course.toUpperCase()
+    $("#contact-section ul")[0].innerHTML = `
+        <li>${data.adress}</li>
+        <li>${data.email}</li>
+        <a target="blank" href="${data.linkedin_url}">
+            <li>
+                ${data.linkedin_name}
+            </li>
+        </a>
+    `
+}
+getPersonalData()
+
+function getAbilities() {
+    let url = "/abilities"
+
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("GET", url, false)
+    xhttp.send()
+
+    let data = JSON.parse(xhttp.responseText)
+
+    data.forEach(ability => {
+        console.log(ability)
+        $("#abilities-section ul")[0].innerHTML += `<li>${ability.name}</li>`
+    });
+}
+getAbilities()
+
+// let hobbies = [
+//     "Ler Livros",
+//     "Futebol",
+//     "Estudar",
+//     "Participar de Projetos",
+//     "Sair com os amigos"
+// ]
+
+// let abilities = [
+//     "Linguagens (HTML, CSS, Python, Javascript e React).",
+//     "Comunicação.",
+//     "Trabalho em Equipe.",
+//     "Liderança.",
+//     "Proatividade.",
+//     "Pensamento Crítico.",
+//     "Escuta ativo.",
+//     "Criatividade para criação de Soluções Tecnológicas"
+// ]
+
+// hobbies.forEach(item => {
+//     $("#hobbies-section ul")[0].innerHTML += `<li>${item}</li>`
+// })
+
+// abilities.forEach(item => {
+//     $("#abilities-section ul")[0].innerHTML += `<li>${item}</li>`
+// })
