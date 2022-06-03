9ef0e8e31d6d2fad9a297bcdb41c3b63f87d3fca
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-02 21:38:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path')
-const port = 3000;
+const PORT = process.env.PORT || 5000
 const sqlite3 = require('sqlite3').verbose()
 const DBPATH = path.join(__dirname, "curriculo.db")
 
@@ -90,6 +90,6 @@ app.get('/educationalhistory', (req, res) => {
 
 
 /* Inicia o servidor */
-app.listen(port, () => {
-  console.log(`Server running on ${port}/`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
 });
